da222dddd458cbab203167933310d00491471273
Nate Jackson <551+njackson@users.noreply.github.com>
2026-02-09 22:14:23 -0800

    feat: add toKebabCase utility

--- packages/aver/src/cli/scaffold.ts
+++ packages/aver/src/cli/scaffold.ts
@@ -1,3 +1,10 @@
+export function toKebabCase(str: string): string {
+  return str
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .replace(/([A-Z])([A-Z][a-z])/g, '$1-$2')
+    .toLowerCase()
+}
+
 export function initProjectFiles(_dir: string): void {
   throw new Error('Not implemented')
 }
